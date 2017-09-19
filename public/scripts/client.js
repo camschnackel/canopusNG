78d1d7368f9156aaaa00774092bd5cfcb4b05d31
Cam <cam.schnackel@gmail.com>
2017-09-19 14:40:49 -0500

    step01

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,9 @@
 console.log( 'js' );
+
+// angular module
+var myApp = angular.module('myApp', []);
+
+// controller for this app
+myApp.controller('ReviewController', function(){
+    console.log('NG');
+})
