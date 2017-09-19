e491f356ec48d916dad114e6ea2c47a4a397a6c2
Cam <cam.schnackel@gmail.com>
2017-09-19 14:49:59 -0500

    step02

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,4 +6,14 @@ var myApp = angular.module('myApp', []);
 // controller for this app
 myApp.controller('ReviewController', function(){
     console.log('NG');
+    // hold controller in a var
+    var vm = this;
+    // array for all bands
+    vm.bands = [];
+    // add band
+    vm.addBand = function () {
+        console.log('in addBand', vm.bandIn);
+        vm.bands.push(vm.bandIn);
+        console.log(vm.bands);
+    }
 })
